16d02bf7828ccc70d8d4b5d0495f08e26094da02
greenSkin <james@positivechoices.com>
2010-05-05 16:09:15 -0700

    Cleaner JavaScript for the menu bar.

--- js/menubar.js
+++ js/menubar.js
@@ -3,94 +3,67 @@
 Drupal.OfflineSignup = Drupal.OfflineSignup || {};
 
 Drupal.behaviors.offlineSignupMenuBar = function(context) {
-  if (Drupal.OfflineSignup.menuBar == undefined) {
-    Drupal.OfflineSignup.menuBar = new Drupal.OfflineSignup.MenuBar();
-  }
-
-  // It's possible more than one menu bar is present. We need to link all menu
-  // bars together so how one is used is reflected in all other bars.
-  $('.offline-signup-menu-bar:not(.offline-signup-menu-bar-processed)', context).each(function() {
-    $menuBar = $(this);
-
-    // Store the bar jQuery object with the main menuBar object.
-    Drupal.OfflineSignup.menuBar.bars.push($menuBar);
-
-    // Loop through the list items for the bar.
-    $('li', $menuBar).each(function() {
-      // Determine what tab type the list item is. Only apply the click event
-      // if the type is successfully determined.
-      var type = Drupal.OfflineSignup.determineTabType($(this));
-      if (type) {
-        $('a', $(this)).click(function() {
-          // Determine if there was a previously active tab.
-          var prevType = Drupal.OfflineSignup.determineTabType($('li.active', $menuBar));
-          if (prevType) {
-            // Trigger the tab blur event for the previous tab type.
-            Drupal.OfflineSignup.menuBar.tabs[prevType].blur();
-          }
-
-          // Loop through each bar element to globally affect active tabs.
-          for (var i in Drupal.OfflineSignup.menuBar.bars) {
-            var bar = Drupal.OfflineSignup.menuBar.bars[i];
-            if (prevType) {
-              $('li.' + prevType + ' a', bar).removeClass('active').parent().removeClass('active');
-            }
-            $('li.' + type + ' a', bar).addClass('active').parent().addClass('active');
-          }
-
-          // Trigger the tab focus event for the clicked tab's type.
-          Drupal.OfflineSignup.menuBar.tabs[type].focus();
-        });
-      }
-    });
-
+  if ($('#offline-signup-menu-bar:not(.offline-signup-menu-bar-processed)').size()) {
+    Drupal.OfflineSignup.menuBar = new Drupal.OfflineSignup.MenuBar($('#offline-signup-menu-bar').get());
     $(this).addClass('offline-signup-menu-bar-processed');
-  });
+  }
+}
+
+Drupal.OfflineSignup.MenuBar = function(element) {
+  this.element = element;
+  this.tabs = {};
+
+  for (var i in Drupal.settings.offlineSignup.menuTabs) {
+    var type = Drupal.settings.offlineSignup.menuTabs[i];
+    this.tabs[type] = new Drupal.OfflineSignup.Tab(type, this);
+  }
 
   // Set the active tab.
   var url = document.location.toString();
   if (url.match('#')) {
     // Make tab active based on anchor if it isn't already active.
     var anchor = '#' + url.split('#')[1];
-    if (!$('a[href="' + anchor + '"]').hasClass('active')) {
-      $('a[href="' + anchor + '"]').click();
+    if (!$('a[href="' + anchor + '"]', $(this.element)).hasClass('active')) {
+      $('a[href="' + anchor + '"]', $(this.element)).click();
     }
   }
 }
 
-Drupal.OfflineSignup.determineTabType = function(li) {
-  if (li.hasClass('signup')) {
-    return 'signup';
-  }
-  else if (li.hasClass('settings')) {
-    return 'settings';
-  }
-  else if (li.hasClass('winners')) {
-    return 'winners';
-  }
-  else if (li.hasClass('sync')) {
-    return 'sync';
-  }
+Drupal.OfflineSignup.Tab = function(type, menuBar) {
+  this.type = type;
+  this.menuBar = menuBar;
+  this.element = $('li.' + this.type + ' a', $(this.menuBar.element)).get();
+
+  var self = this;
+
+  $(this.element).click(function() {
+    // Determine if there was a previously active tab.
+    var prevType = self.determineType($('li.active', $(self.menuBar.menuElement)));
+    if (prevType) {
+      // Trigger the tab blur event for the previous tab.
+      self.menuBar.tabs[prevType].blur();
+    }
+
+    // Trigger the tab focus event for the clicked tab.
+    self.focus();
+  });
 }
 
-Drupal.OfflineSignup.MenuBar = function() {
-  this.bars = new Array();
-  this.tabs = {};
-
+Drupal.OfflineSignup.Tab.prototype.determineType = function(element) {
   for (var i in Drupal.settings.offlineSignup.menuTabs) {
     var type = Drupal.settings.offlineSignup.menuTabs[i];
-    this.tabs[type] = new Drupal.OfflineSignup.Tab(type);
+    if (element.hasClass(type)) {
+      return type;
+    }
   }
-}
-
-Drupal.OfflineSignup.Tab = function(type) {
-  this.type = type;
 }
 
 Drupal.OfflineSignup.Tab.prototype.blur = function() {
   $('#offline-signup-content-' + this.type).hide();
+  $(this.element).removeClass('active').parent().removeClass('active');
 }
 
 Drupal.OfflineSignup.Tab.prototype.focus = function() {
+  $(this.element).addClass('active').parent().addClass('active');
   $('#offline-signup-content-' + this.type).show();
 }
