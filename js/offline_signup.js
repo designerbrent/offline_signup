755ded9d935cc6e6db3f234571678c8ab69c9e3c
greenSkin <james@positivechoices.com>
2010-05-10 10:38:35 -0700

    Moved signup JavaScript to separate file.

--- js/offline_signup.js
+++ js/offline_signup.js
@@ -12,61 +12,6 @@ Drupal.behaviors.offlineSignup = function(context) {
 
     Drupal.OfflineSignup.users = Drupal.settings.offlineSignup.users;
 
-    var $routerForm = $('#offline-signup-user-router-form');
-    var $registerForm = $('#offline-signup-user-register-form');
-    var $updateForm = $('#offline-signup-user-update-form');
-
-    // Router form.
-    $('.form-submit', $routerForm).click(function() {
-      var $mailInput = $('input[name=mail]', $routerForm);
-      var mail = $mailInput.val();
-      if (Drupal.OfflineSignup.mailValid(mail)) {
-        if ($mailInput.hasClass('error')) {
-          $mailInput.removeClass('error');
-        }
-
-        if (Drupal.OfflineSignup.mailTaken(mail)) {
-          $routerForm.hide();
-          $('input[name=mail]', $updateForm).val(mail);
-          $updateForm.show();
-        }
-        // E-mail address is available.
-        else {
-          $routerForm.hide();
-          $('input[name=mail]', $registerForm).val(mail);
-          $registerForm.show();
-        }
-      }
-      // Invalid e-mail address.
-      else {
-        $mailInput.addClass('error');
-        alert(Drupal.t('Invalid e-mail address. Please enter a valid e-mail address.'));
-      }
-      return false;
-    });
-
-    // Register form.
-    $('input[name=mail]', $registerForm).focus(function() {
-      $('input[name=back]', $registerForm).click();
-      $('input[name=mail]', $routerForm).focus();
-    });
-    $('input[name=back]', $registerForm).click(function() {
-      $registerForm.hide();
-      $routerForm.show();
-      return false;
-    });
-
-    // Update form.
-    $('input[name=mail]', $updateForm).focus(function() {
-      $('input[name=back]', $updateForm).click();
-      $('input[name=mail]', $routerForm).focus();
-    });
-    $('input[name=back]', $updateForm).click(function() {
-      $updateForm.hide();
-      $routerForm.show();
-      return false;
-    });
-
     // Reveal the page.
     $('#offline-signup-page').show();
 
@@ -86,14 +31,6 @@ Drupal.OfflineSignup.mailTaken = function(mail) {
 Drupal.OfflineSignup.mailValid = function(mail) {
   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   return reg.test(mail);
-}
-
-Drupal.OfflineSignup.NewUser = function() {
-  
-}
-
-Drupal.OfflineSignup.UpdateUser = function() {
-  
 }
 
 /**
