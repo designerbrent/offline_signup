266ebeefdfe6bb57fd6955a797e4dfc9790c6a8b
greenSkin <james@positivechoices.com>
2010-05-27 16:24:47 -0700

    Added node forms.

---
+++ js/profiles.js
@@ -0,0 +1,19 @@
+// $Id$
+
+Drupal.OfflineSignup = Drupal.OfflineSignup || {};
+
+Drupal.behaviors.offlineSignupProfiles = function() {
+  
+}
+
+Drupal.OfflineSignup.Profiles = function() {
+  
+}
+
+Drupal.OfflineSignup.Profiles.prototype.list = function(user) {
+  
+}
+
+Drupal.OfflineSignup.Profile = function() {
+  
+}
